774cb4d7b6bc81fe39ab74c7b2d093dcad256495
Heather Cunningham <hcunningham@patientkeeper.com>
2021-12-09 17:58:55 -0500

    corrected my pug, sass, and added cover image from tutorial

--- lib/screens/homepage/homepage.dart
+++ lib/screens/homepage/homepage.dart
@@ -2,8 +2,6 @@
 import 'package:flutter/material.dart';
 // ignore: unused_import
 import 'package:flutter/cupertino.dart';
-
-
 // ignore: non_constant_identifier_names
 Scaffold HomePage() {
   return Scaffold( // project://lib\screens\homepage\homepage.pug#2,5
@@ -16,20 +14,41 @@ Scaffold HomePage() {
         ),
       ),
     ),
-    body: 
-    //-- GREETING ----------------------------------------------------------
-    Center( // project://lib\screens\homepage\homepage.pug#5,9
-      // fontSize: '24px',
-      // fontWeight: 'bold',
-      child: Text( 
-        'Hello Flutter-View World!',
-        style: TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-          color: Colors.blue
+    body: Center( // project://lib\screens\homepage\homepage.pug#5,9
+      child: FittedBox( // project://lib\screens\homepage\homepage.pug#6,13
+        child: Column( 
+          children: __flatten([
+
+            //-- COVER ----------------------------------------------------------
+            Container( // project://lib\screens\homepage\homepage.pug#7,17
+              decoration: BoxDecoration( 
+                image: DecorationImage( 
+                  image: NetworkImage( 
+                    'https://cutt.ly/Gat1ivy',
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              width: 300,
+              height: 300,
+            ),
+            DefaultTextStyle.merge( 
+              child: 
+              //-- GREETING ----------------------------------------------------------
+              Container( // project://lib\screens\homepage\homepage.pug#9,17
+                child: Text( 
+                  'Hello Flutter-View World!',
+                ),
+              ),
+              style: TextStyle( 
+                fontSize: 24,
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ]),
         ),
       ),
-      // color: Colors.blue,
     ),
   );
 }
